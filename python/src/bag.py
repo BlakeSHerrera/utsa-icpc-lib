df20ec47e24497298b14b2dfb7dbe48c1b207193
Blake Herrera <blakeherrera1@gmail.com>
2026-08-19 05:56:34 -0500

    Add Bag tests and abstractions

--- python/src/bag.py
+++ python/src/bag.py
@@ -2,10 +2,29 @@ import abc
 import collections
 from typing import Iterable, TypeVar, Generic
 
+import view
+
 
 T = TypeVar('T')
 
-class Bag(abc.ABC, Generic[T]):
+
+class BagView(abc.ABC, Generic[T]):
+    '''A BagView is the immutable data type of the mutable Bag (see below).'''
+
+    def __len__(self) -> int:
+        '''Check the size of the bag via len(my_bag).'''
+        ...
+    
+    def __bool__(self) -> bool:
+        '''A bag is False-y if it is empty.'''
+        return bool(len(self))
+
+    def peek(self) -> T:
+        '''Equivalent to pop() without removing the item.'''
+        ...
+
+    
+class Bag(BagView[T], abc.ABC):
     '''
     A Bag is a data structure in which an item can be inserted
     (pushed) and then an item can be taken out (popped) according
@@ -21,7 +40,7 @@ class Bag(abc.ABC, Generic[T]):
         '''Insert an item into the Bag.'''
         ...
 
-    def push_many(self, items: Iterable[T]):
+    def push_all(self, items: Iterable[T]):
         '''Insert many items into the Bag one by one.'''
         for i in items:
             self.push(i)
@@ -30,49 +49,66 @@ class Bag(abc.ABC, Generic[T]):
         '''Remove an item from the bag.'''
         ...
 
-    def peek(self) -> T:
-        '''Equivalent to pop() without removing the item.'''
-        ...
-
-    def __len__(self) -> int:
-        '''Check the size of the bag via len(my_bag).'''
-        ...
-
-    def __bool__(self) -> bool:
-        '''A bag is False-y if it is empty.'''
-        return bool(len(self))
-
     def __iter__(self) -> Iterable[T]:
         '''Iterating through the bag empties it.'''
         while self:
             yield self.pop()
 
 
-class DequeBag(Bag[T], abc.ABC):
+class DequeBag(view.DequeView, Bag[T], abc.ABC):
 
     def __init__(self, items: Iterable[T] = ()):
-        self._deque = collections.deque(items)
+        super().__init__(collections.deque())
+        self.push_all(items)
 
-    def __len__(self):
-        return len(self._deque)
+    __iter__ = Bag.__iter__
     
     def push(self, item: T):
-        self._deque.append(item)
+        self._data.append(item)
 
 
-class Stack(DequeBag[T]):
+class ListBag(view.ListView, Bag[T], abc.ABC):
+
+    def __init__(self, items: Iterable[T] = ()):
+        super().__init__(list())
+        self.push_all(items)
+
+    __iter__ = Bag.__iter__
+
+    def push(self, item: T):
+        self._data.append(item)
+
+
+class Stack(Bag[T], abc.ABC):
+    pass
+
+
+class DequeStack(DequeBag[T], Stack[T]):
 
     def pop(self) -> T:
-        return self._deque.pop()
+        return self._data.pop()
 
     def peek(self) -> T:
-        return self._deque[-1]
+        return self[-1]
+
+
+class ListStack(ListBag[T], Stack[T]):
+
+    def pop(self) -> T:
+        return self._data.pop()
+
+    def peek(self) -> T:
+        return self[-1]
+
     
+class Queue(Bag[T], abc.ABC):
+    pass
 
-class Queue(DequeBag[T]):
+
+class DequeQueue(DequeBag[T], Queue[T]):
 
     def pop(self) -> T:
-        return self._deque.popleft()
+        return self._data.popleft()
 
     def peek(self) -> T:
-        return self._deque[0]
+        return self[0]
--- python/tests/test_bag.py
+++ python/tests/test_bag.py
@@ -1,15 +1,93 @@
-from bag import Stack
+from bag import *
 
 import pytest
 
 
-def test_stack():
-    stack = Stack()
-    stack.push(1)
-    stack.push(2)
-    assert stack.pop() == 2
-    assert stack.pop() == 1
+N = 5
+R = range(N)
 
 
-if __name__ == '__main__':
-    test_stack()
+@pytest.fixture
+def deque_queue() -> DequeQueue:
+    return DequeQueue()
+
+@pytest.fixture(params = [deque_queue], ids = ['deque'])
+def queue(request: pytest.FixtureRequest) -> Queue:
+    return request.getfixturevalue(request.param.__name__)
+
+@pytest.fixture
+def deque_stack() -> DequeStack:
+    return DequeStack()
+
+@pytest.fixture
+def list_stack() -> ListStack:
+    return ListStack()
+
+@pytest.fixture(params = [list_stack, deque_stack], ids = ['list', 'deque'])
+def stack(request: pytest.FixtureRequest) -> Stack:
+    return request.getfixturevalue(request.param.__name__)
+
+@pytest.fixture(params = [deque_queue, list_stack, deque_stack], ids = ['queue', 'list_stack', 'deque_stack'])
+def bag(request: pytest.FixtureRequest) -> Bag:
+    return request.getfixturevalue(request.param.__name__)
+
+
+# ===== abstract base class tests =====
+
+def test_len(bag: Bag):
+    assert len(bag) == 0
+    bag.push(1)
+    assert len(bag) == 1
+
+def test_bool(bag: Bag):
+    assert bool(bag) == False
+    bag.push(1)
+    assert bool(bag) == True
+
+def test_peek(bag: Bag):
+    with pytest.raises(IndexError):
+        bag.peek()
+    bag.push(1)
+    assert bag.peek() == 1
+    assert bag.peek() == 1
+
+def test_push(bag: Bag):
+    for i in R:
+        assert len(bag) == i
+        bag.push(i)
+
+def test_push_all(bag: Bag):
+    bag.push_all(R)
+    assert len(bag) == len(R)
+
+def test_pop(bag: Bag):
+    bag.push_all(R)
+    for i in R:
+        assert len(bag) == N - i
+        bag.pop()
+    assert len(bag) == 0
+
+def test_peek_pop(bag: Bag):
+    bag.push_all(R)
+    for i in R:
+        assert bag.peek() == bag.pop()
+
+def test_iter(bag: Bag):
+    bag.push_all(R)
+    iter_ = iter(bag)
+    for _ in R:
+        value = bag.peek()
+        assert value == next(iter_)
+    with pytest.raises(StopIteration):
+        next(iter_)
+
+
+# ===== Stack / Queue =====
+
+def test_stack(stack: Stack):
+    stack.push_all(R)
+    assert list(stack) == list(R)[::-1]
+
+def test_queue(queue: Queue):
+    queue.push_all(R)
+    assert list(queue) == list(R)
