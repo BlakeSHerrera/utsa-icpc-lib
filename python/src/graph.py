227dd90380179a29e0db180f68636e3fff9237c8
Blake Herrera <blakeherrera1@gmail.com>
2026-08-21 21:49:00 -0500

    Create basic Index classes

--- python/src/graph.py
+++ python/src/graph.py
@@ -1,8 +1,12 @@
 
+
 import abc
+import collections, collections.abc
 import enum
 from numbers import Number
-from typing import Any, Self, TypeVar
+from typing import Any, Iterable, Self, TypeVar
+
+import utils
 
 
 T = TypeVar('T')
@@ -56,3 +60,55 @@ class WeightedEdge(Edge):
     def __init__(self, from_: Node, to: Node, weight: Number, **kwargs):
         super().__init__(from_, to, **kwargs)
         self.weight = weight
+
+
+class Index(abc.ABC):
+
+    def add_node(self, node: Node):
+        raise NotImplementedError
+
+    def add_edge(self, edge: Edge):
+        raise NotImplementedError
+
+    def remove_node(self, node: Node):
+        raise NotImplementedError
+
+    def remove_edge(self, edge: Edge):
+        raise NotImplementedError
+
+
+class CompositeIndex(Index):
+
+    def __init__(self, indices: Iterable[Index]):
+        self.indices = tuple(indices)
+
+    def add_node(self, node: Node):
+        for i in self.indices:
+            i.add_node(node)
+
+    def add_edge(self, edge: Edge):
+        for i in self.indices:
+            i.add_edge(edge)
+
+    def remove_node(self, node: Node):
+        for i in self.indices:
+            i.remove_node(node)
+
+    def remove_edge(self, edge: Edge):
+        for i in self.indices:
+            i.remove_edge(edge)
+
+    
+class LaxIndex(Index, abc.ABC):
+
+    def add_node(self, node: Node):
+        pass
+
+    def add_edge(self, edge: Edge):
+        pass
+
+    def remove_node(self, node: Node):
+        pass
+
+    def remove_edge(self, edge: Edge):
+        pass
