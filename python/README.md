57b1008c2f26b2c46da64d48784f9866b84d57d0
Blake Herrera <blakeherrera1@gmail.com>
2026-09-11 20:26:43 -0500

    Add some documentation (#5)
    Created readme and added basic docstrings.

---
+++ python/README.md
@@ -0,0 +1,72 @@
+# UTSA ICPC Lib (Python)
+
+This is an instructive library meant for teaching and learning about data structures and algorithms used in competitive programming contests and similarly structured websites like LeetCode and Kattis. The focus is not on performance of the specific implementations, but abstracting the concepts elegantly so that they can be reused and related to one another, increasing the student's understanding of them as a whole. A problem that requires strict performance tuning may require rewriting a more efficient implementation or even using a faster language such as C.
+
+## Instructional Usage
+
+Because the data structures and algorithms are prebuilt in this library, the student can focus on how they are used in solving problems without worrying about they are implemented.
+
+Another way to use the library is to remove the implementation of a particular data structure or algorithm, and have the student build it from scratch. Then the suite of unit tests can be run via simply running `pytest` to validate the code.
+
+This library can also be used to show how to decompose problems and structure them in code so that a prebuilt algorithm can solve them.
+
+## Usage
+
+Add the `./python/src` folder to your `PYTHONPATH`. This library is built on Python 3.11, which is currently used by the PyPy project and thus some competitive programming sites such as Kattis. No external libraries are included.
+
+Most competitive programming formats flatten all folder structures, and for this reason, no folder structure exists within the source folder.
+
+Developers will want to create a virtual environment via:
+- `python -m venv .venv` (`.venv` is the name of the virtual environment folder - you can change this if you wish.)
+- Activate the virtual environment:
+  - Linux / MacOS:
+    - bash / zsh: `source .venv/scripts/activate`
+    - fish: `source .venv/scripts/activate.fish`
+    - csh / tcsh: `source .venv/scripts/activate.csh`
+  - Windows
+    - Command Prompt (cmd): `.venv\scripts\activate.bat`
+    - PowerShell: `.\.venv\scripts\Activate.ps1`
+    - Git Bash: `source .venv/scripts/activate` (note bash above)
+- `python -m pip install ./python/src/requirements_dev.txt`
+
+## Project Structure
+
+### bag
+
+This module contains implementations of "bags", which are objects that fundamentally have two operations: putting an item in (a `push`), and taking an item out (a `pop`). This library also includes `peek` to get the next item without `pop`ping it, the size via the `len()` function, and truthiness dependent on whether the bag is empty or not.
+
+Common types of bags are the `Stack`, the `Queue`, and the `Heap` (sometimes called a `PriorityQueue`). Because there are multiple ways to implement these, the concrete classes include the name of the implementation used (such as a `ListHeap` using a `list` to create the heap).
+
+### graph
+
+A graph is a data structure that consists of nodes and edges that connect two nodes.
+
+This library treats graphs as directed, weighted multigraphs. That is, all edges have directions (directed graph), and all edges have weight (weighted graph). Edges can connect the same node to itself, and multiple edges between the same nodes are allowed (multigraph). Read on to see how to simulate an undirected or unweighted graph.
+
+An unweighted graph is very easy to implement. Algorithms can simply ignore the edge weight. By default, edges all have a weight of 1, which could potentially be used to count the number of steps taken in a traversal.
+
+Similarly, to create an undirected graph, the edge direction can be ignored. Traversal is done with the `Direction` enum, which has the values `OUT`, `IN`, and `BOTH`. Thus, an undirected graph can be thought of as a graph where there is no edge `(u, v)` with a counterpart `(v, u)`, and the algorithm can simply utilize `Direction.BOTH` to traverse.
+
+#### Abstract Base Classes
+
+Importantly, a `Graph` is an abstract class. That is, it defines the methods that are available (such as getting the nodes, edges, and neighbors, as well as adding/removing them), but how the information is stored and retrieved is an implementation detail for subclasses. A graph can be constructed many ways (like the `bag`s above); currently there is `AdjacencySet`, `ElementSet`, and `EdgeLookup` as concrete classes.
+
+The graph subclasses do not implement all of the methods if they are inefficient in doing so. For example, the `EdgeLookup` class is designed solely to get a list of edges from node `u` to `v` in constant time, but cannot efficiently count the number of edges. Trying to call a method which is not implemented will raise a `NotImplementedError` exception at runtime.
+
+A programmer could combine the benefits and drawbacks of different implementations by creating a subclass composed of multiple concrete implementations (a composite graph model). Adding or removing a node or edge is broadcast to the underlying implementations, and the best implementation is picked for each method, such as the edge lookup question vs. the number of edges question.
+
+### grid
+
+This module provides helpers to turn a matrix into a graph for use in graph algorithms. There are also helpers to get nodes by their row and column. Note that these grid graphs can be connected in interesting ways such as knight movements.
+
+This module also defines a `Point` class which is a helper around x/y and row/col representations of coordinates, since they are inverses of each other. These are integer coordinates.
+
+### utils
+
+Various utilities for common programming patterns not offered by the standard library.
+
+### view
+
+Provides wrappers around base data structures to provide data-reading operations for built-in types without being able to modify them.
+
+This philosophy extends around the project with views and mutable classes. Classes with more specific constraints can be viewed as the higher-order class but not modified like one. For example, a tree can be viewed as a graph for the purposes of performing graph algorithms like a BFS traversal, however, it has constraints that mean it cannot be modified at-will like any graph.
--- python/src/graph.py
+++ python/src/graph.py
@@ -1,3 +1,21 @@
+'''
+A graph is a data structure that consists of nodes and edges that connect two nodes.
+
+This library treats graphs as directed, weighted multigraphs. That is, all edges have 
+directions (directed graph), and all edges have weight (weighted graph). Edges can 
+connect the same node to itself, and multiple edges between the same nodes are allowed 
+(multigraph). Read on to see how to simulate an undirected or unweighted graph.
+
+An unweighted graph is very easy to implement. Algorithms can simply ignore the edge 
+weight. By default, edges all have a weight of 1, which could potentially be used to 
+count the number of steps taken in a traversal.
+
+Similarly, to create an undirected graph, the edge direction can be ignored. Traversal 
+is done with the `Direction` enum, which has the values `OUT`, `IN`, and `BOTH`. Thus, 
+an undirected graph can be thought of as a graph where there is no edge `(u, v)` with a 
+counterpart `(v, u)`, and the algorithm can simply utilize `Direction.BOTH` to traverse.
+'''
+
 
 from __future__ import annotations
 import abc
@@ -11,12 +29,23 @@ import utils
 
 
 class Node:
+    '''
+    A node is a type of element in a graph. Nodes are connected by edges.
+    Nodes typically carry some kind of data (at least a name or label typically).
+    '''
 
     def __init__(self, data: Any):
         self.data = data
 
 
 class Edge:
+    '''
+    An edge connects two nodes and has a defined weight (1 by default).
+    The edge has a defined `from_` and `to` direction, so edges are directed.
+
+    Two edges with the same `from_` and `to` nodes and same weight are not equivalent.
+    Multiple edges are allowed between the same pair of nodes.
+    '''
 
     def __init__(self, from_: Node, to: Node, weight: Real = 1):
         self.from_ = from_
@@ -24,20 +53,31 @@ class Edge:
         self.weight = weight
 
     def reverse(self) -> Self:
+        '''Return a new edge with the `from_` and `to` nodes reversed.'''
         return Edge(self.to, self.from_)
 
     @property
     def nodes(self) -> tuple[Node, Node]:
+        '''
+        Get the two nodes that the edge connects.
+        The `from_` node is first and the `to` node is last.
+        '''
         return (self.from_, self.to)
 
     @property
     def nodes_r(self) -> tuple[Node, Node]:
+        '''
+        Get the two nodes that the edge connects, in reverse order.
+        The `from_` node is last and the `to` node is first.
+        '''
         return (self.to, self.from_)
 
     def __contains__(self, other: Node) -> bool:
+        '''Return true if the node is one of the nodes that the edge connects.'''
         return other in self.nodes
 
     def neighbors(self, direction: Direction) -> Iterable[Node]:
+        '''Get the nodes that this edge connects, depending on the direction.'''
         match direction:
             case Direction.OUT:
                 return (self.to,)
@@ -48,12 +88,14 @@ class Edge:
 
 
 class Direction(utils.ZeroBasedEnum):
+    '''An enum for defining the edge traversal directions.'''
     
     OUT = enum.auto()
     IN = enum.auto()
     BOTH = enum.auto()
 
     def reverse(self) -> Self:
+        '''Reverse the direction. OUT <-> IN. BOTH remains unchanged.'''
         match self:
             case Direction.OUT:
                 return Direction.IN
@@ -63,6 +105,12 @@ class Direction(utils.ZeroBasedEnum):
                 return BOTH
 
     def orient(self, edge: Edge) -> Iterable[tuple[Node, Node]]:
+        '''
+        Get the nodes for an edge along this direction in traversal order. That is,
+        OUT: (from_, to)
+        IN: (to, from_)
+        BOTH: (from_, to) and (to, from_)
+        '''
         match self:
             case Direction.OUT:
                 return (edge.nodes,)
@@ -70,64 +118,158 @@ class Direction(utils.ZeroBasedEnum):
                 return (edge.nodes_r,)
             case Direction.BOTH:
                 return (edge.nodes, edge.nodes_r)
-            
+
 
 class GraphView:
+    '''An immutable view composed of an underlying concrete implementation.'''
 
     def __init__(self, graph: GraphView):
         self._graph = graph
 
     def neighbors(self, direction: Direction, element: Node | Edge) -> Iterable[Node | Edge]:
+        '''Get the next elements given an element and a traversal direction.'''
         if isinstance(element, Edge):
             return element.neighbors(direction)
         return self._neighbors(direction, element)
 
     # Thin wrappers
-    def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]: return self._graph._neighbors(direction, node)
-    def degree(self, direction: Direction, node: Node) -> int: return self._graph.degree(direction, node)
-    def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: return self._graph.edges_between(from_, to)
-    def nodes(self) -> Iterable[Node]: return self._graph.nodes()
-    def edges(self) -> Iterable[Edge]: return self._graph.edges()
-    def v(self) -> int: return self._graph.v()
-    def e(self) -> int: return self._graph.e()
-    def __bool__(self) -> bool: return bool(self._graph)
+    def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]:
+        '''Get all the neighbors of a node in a paticular direction.'''
+        return self._graph._neighbors(direction, node)
+
+    def degree(self, direction: Direction, node: Node) -> int:
+        '''
+        Get the in-degree, out-degree, or total degree of the node, which is
+        the number of edges connected to the node in that direction.
+        '''
+        return self._graph.degree(direction, node)
+
+    def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: 
+        '''Get all the edges between the two nodes.'''
+        return self._graph.edges_between(from_, to)
+    
+    def nodes(self) -> Iterable[Node]: 
+        '''Get all of the nodes of the graph.'''
+        return self._graph.nodes()
+
+    def edges(self) -> Iterable[Edge]: 
+        '''Get all of the edges of the graph.'''
+        return self._graph.edges()
+    
+    def v(self) -> int:
+        '''Get the number of verticies (nodes) in the graph.'''
+        return self._graph.v()
+    
+    def e(self) -> int: 
+        '''Get the number of edges in the graph.'''
+        return self._graph.e()
+    
+    def __bool__(self) -> bool: 
+        '''A graph is false-y if it contains no nodes (and therefore no edges).'''
+        return bool(self._graph)
 
     def __eq__(self, other: GraphView) -> bool:
-        return set(self.nodes()) == set(other.nodes()) and set(self.edges()) == set(other.edges())
+        '''Two graphs are equal if their sets of nodes and edges are equal.'''
+        return set(self.nodes()) == set(other.nodes()) \
+            and set(self.edges()) == set(other.edges())
     
 class MutableGraph(GraphView, abc.ABC):
+    '''A mutable graph is a graph that can be mutated and changed.'''
 
     def __init__(self, nodes: Iterable[Node] = (), edges: Iterable[Edge] = ()):
         super().__init__(self)
         self.add_all(nodes, edges)
 
-    def add_node(self, node: Node): raise NotImplementedError
-    def add_edge(self, edge: Edge): raise NotImplementedError
-    def remove_node(self, node: Node): raise NotImplementedError
-    def remove_edge(self, edge: Edge): raise NotImplementedError
+    def add_node(self, node: Node): 
+        '''Add a node into the graph.'''
+        raise NotImplementedError
+    
+    def add_edge(self, edge: Edge): 
+        '''Add an edge into the graph.'''
+        raise NotImplementedError
+    
+    def remove_node(self, node: Node): 
+        '''Remove a node from the graph.'''
+        raise NotImplementedError
+    
+    def remove_edge(self, edge: Edge): 
+        '''Remove an edge from the graph.'''
+        raise NotImplementedError
     
     def add_all(self, nodes: Iterable[Node] = (), edges: Iterable[Edge] = ()):
+        '''Add all of the given nodes and edges from the graph.'''
         utils.apply(self.add_node, nodes)
         utils.apply(self.add_edge, edges)
 
-    def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]: raise NotImplementedError
-    def degree(self, direction: Direction, node: Node) -> int: raise NotImplementedError
-    def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: raise NotImplementedError
-    def nodes(self) -> Iterable[Node]: raise NotImplementedError
-    def edges(self) -> Iterable[Edge]: raise NotImplementedError
-    def v(self) -> int: raise NotImplementedError
-    def e(self) -> int: raise NotImplementedError
-    def __bool__(self) -> bool: raise NotImplementedError
+    def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]: 
+        '''Get the edges that connect a node in the given direction.'''
+        raise NotImplementedError
+
+    def degree(self, direction: Direction, node: Node) -> int: 
+        '''Get the in-degree, out-degree, or total degree of the node.'''
+        raise NotImplementedError
+
+    def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: 
+        '''Get all edges between the two nodes.'''
+        raise NotImplementedError
+
+    def nodes(self) -> Iterable[Node]:
+        '''Get all nodes in the graph.'''
+        raise NotImplementedError
+
+    def edges(self) -> Iterable[Edge]: 
+        '''Get all edges in the graph.'''
+        raise NotImplementedError
+
+    def v(self) -> int: 
+        '''Get the number of nodes (vertices) in the graph.'''
+        raise NotImplementedError
+
+    def e(self) -> int: 
+        '''Get the number of edges in the graph.'''
+        raise NotImplementedError
+
+    def __bool__(self) -> bool: 
+        '''A graph is false-y if it contains no nodes (and therefore no edges either).'''
+        raise NotImplementedError
 
 
 class LaxGraph(MutableGraph, abc.ABC):
-    def add_node(self, node: Node): pass
-    def add_edge(self, edge: Edge): pass
-    def remove_node(self, node: Node): pass
-    def remove_edge(self, edge: Edge): pass
+    '''
+    A LaxGraph (relaxed graph) is one which is a partial implementation of a graph.
+    The underlying mutations do not need to be concretely implemented.
+    For example, the subclass could merely count only the number of nodes and
+    therefore ignore any operations happening with edges.
+    '''
+
+    def add_node(self, node: Node):
+        '''Add a node into the graph (this operation has no effect).''' 
+        pass
+
+    def add_edge(self, edge: Edge): 
+        '''Add an edge into the graph (this operation has no effect).'''
+        pass
+
+    def remove_node(self, node: Node): 
+        '''Remove a node from the graph (this operation has no effect).'''
+        pass
+
+    def remove_edge(self, edge: Edge): 
+        '''Remove an edge from t he graph (this operation has no effect).'''
+        pass
 
 
 class AdjacencySet(MutableGraph):
+    '''
+    An adjacency set keeps a mapping between a node and the edges it is connected to.
+    This class keeps such a mapping for each of the three traversal directions.
+
+    This type of graph is ideal for traversal, getting nodes and neighbors, and finding 
+    the degree of a node.
+
+    This type of graph is not suited for telling whether an edge exists in the graph or
+    for getting the edges between two nodes.
+    '''
 
     def __init__(self, nodes: Iterable[Node] = (), edges: Iterable[Edge] = ()):
         self._adjacency: Mapping[Direction, dict[Node, set[Edge]]] = [dict() for _ in Direction]
@@ -135,11 +277,13 @@ class AdjacencySet(MutableGraph):
         super().__init__(nodes, edges)
     
     def add_node(self, node: Node):
+        '''Add a node into the graph.'''
         for dir in Direction:
             self._adjacency[dir][node] = set()
         self._self_edges[node] = set()
     
     def add_edge(self, edge: Edge):
+        '''Add an edge into the graph.'''
         for dir in Direction:
             for node in edge.neighbors(dir.reverse()):
                 self._adjacency[dir][node].add(edge)
@@ -147,11 +291,13 @@ class AdjacencySet(MutableGraph):
             self._self_edges[edge.to].add(edge)
 
     def remove_node(self, node: Node):
+        '''Remove a node from the graph.'''
         for dir in Direction:
             del self._adjacency[dir][node]
         del self._self_edges[node]
 
     def remove_edge(self, edge: Edge):
+        '''Remove an edge from the graph.'''
         for dir in Direction:
             for node in edge.neighbors(dir.reverse()):
                 self._adjacency[dir][node].remove(edge)
@@ -159,11 +305,16 @@ class AdjacencySet(MutableGraph):
             self._self_edges[edge.to].remove(edge)
 
     def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]:
+        '''Get the edges that connect a node in the given direction.'''
         if direction is Direction.BOTH:
             return itertools.chain(self._adjacency[direction][node], self._self_edges[node])
         return self._adjacency[direction][node]
 
     def degree(self, direction: Direction, node: Node) -> int:
+        '''
+        Get the in-degree, out-degree, or total degree of the node, which is
+        the number of edges connected to the node in that direction.
+        '''
         if direction is Direction.BOTH:
             return len(self._adjacency[direction][node]) + len(self._self_edges[node])
         return len(self._adjacency[direction][node])
@@ -171,25 +322,41 @@ class AdjacencySet(MutableGraph):
     # def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: raise NotImplementedError
     
     def nodes(self) -> collections.abc.KeysView[Node]:
+        '''Get the nodes in the graph.'''
         return self._adjacency[Direction.OUT].keys()
 
     def edge_sets(self) -> collections.abc.ValuesView[set[Edge]]:
+        '''Get the sets of edges from the adjacency set.'''
         return self._adjacency[Direction.OUT].values()
     
     def edges(self) -> Iterable[Edge]:
+        '''Get the edges in the graph.'''
         return itertools.chain.from_iterable(self.edge_sets())
 
     def v(self) -> int:
+        '''Get the number of nodes (vertices) in the graph.'''
         return len(self._adjacency[Direction.OUT])
 
     def e(self) -> int:
+        '''Get the number of edges in the graph.'''
         return sum(map(len, self.edge_sets()))
 
     def __bool__(self) -> bool:
+        '''A graph is false-y if it contains no nodes (and therefore no edges either).'''
         return bool(self.v())
 
 
 class ElementSet(MutableGraph):
+    '''
+    An element set simply keeps track of the nodes and edges that have been added
+    via sets for each, and a set for both.
+
+    This type of graph is best suited to answer whether a particular element is
+    in the graph, getting the nodes and edges, and counting them.
+
+    This graph is not suited for traversal, getting the degree of nodes, or finding
+    the edges between two nodes.
+    '''
 
     def __init__(self, nodes: Iterable[Node] = (), edges: Iterable[Edge] = ()):
         self._nodes: set[Node] = set()
@@ -198,14 +365,17 @@ class ElementSet(MutableGraph):
         super().__init__(nodes, edges)
 
     def add_node(self, node: Node):
+        '''Add a node to the graph.'''
         self._nodes.add(node)
         self._elements.add(node)
 
     def add_edge(self, edge: Edge):
+        '''Add an edge to the graph.'''
         self._edges.add(edge)
         self._elements.add(edge)
 
     def remove_node(self, node: Node):
+        '''Remove a node from the graph.'''
         self._nodes.remove(node)
         self._elements.remove(node)
         for edge in tuple(self._edges):
@@ -213,33 +383,44 @@ class ElementSet(MutableGraph):
                 self.remove_edge(edge)
 
     def remove_edge(self, edge: Edge):
+        '''Remove an edge from the graph.'''
         self._edges.remove(edge)
         self._elements.remove(edge)
 
-    # def neighbor_nodes(self, direction: Direction, node: Node) -> Iterable[Edge]: raise NotImplementedError
+    # def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]: raise NotImplementedError
     # def degree(self, direction: Direction, node: Node) -> int: raise NotImplementedError
     # def edges_between(self, from_: Node, to: Node) -> Iterable[Edge]: raise NotImplementedError
     
     def nodes(self) -> set[Node]:
+        '''Get the nodes of the graph.'''
         return self._nodes
     
     def edges(self) -> set[Edge]:
+        '''Get the edges of the graph'''
         return self._edges
 
     def elements(self) -> set[Node | Edge]:
+        '''Get a set containing both the nodes and edges of the graph.'''
         return self._elements
 
     def v(self) -> int:
+        '''Get the number of nodes of the graph.'''
         return len(self._nodes)
 
     def e(self) -> int:
+        '''Get the number of edges of the graph.'''
         return len(self._edges)
 
     def __bool__(self) -> bool:
+        '''A graph is false-y if it contains no nodes (and therefore no edges either).'''
         return bool(self._elements)
 
 
 class EdgeSearch(MutableGraph):
+    '''
+    This is a specialized type of graph designed to answer which edges
+    exist between two nodes. It is not suited for most other tasks.
+    '''
 
     def __init__(self, nodes: Iterable[Node] = (), edges: Iterable[Edge] = ()):
         self._nodes = set(nodes)
@@ -248,14 +429,17 @@ class EdgeSearch(MutableGraph):
         super().__init__(nodes, edges)
 
     def add_node(self, node: Node):
+        '''Add a node into the graph.'''
         self._nodes.add(node)
 
     def add_edge(self, edge: Edge):
+        '''Add an edge into the graph.'''
         for dir in Direction:
             for node_1, node_2 in dir.orient(edge):
                 self._edges[dir][node_1][node_2].add(edge)
 
     def remove_node(self, node: Node):
+        '''Remove a node from the graph.'''
         self._nodes.remove(node)
         for dir in Direction:
             second_node_mapping = self._edges[dir].pop(node, ())
@@ -266,30 +450,37 @@ class EdgeSearch(MutableGraph):
             second_node_mapping.pop(node, None)
 
     def remove_edge(self, edge: Edge):
+        '''Remove an edge from the graph.'''
         for dir in Direction:
             for node_1, node_2 in dir.orient(edge):
                 self._edges[dir][node_1][node_2].remove(edge)
 
     def _neighbors(self, direction: Direction, node: Node) -> Iterable[Edge]:
+        '''Get the edges that connect a node in the given direction.'''
         return itertools.chain.from_iterable(self._edges[direction][node].values())
 
     # def degree(self, direction: Direction, node: Node) -> int: raise NotImplementedError
 
     def edges_between(self, from_: Node, to: Node) -> set[Edge]:
+        '''Get the edges that connect a node in the given direction.'''
         return self.edges[Direction.OUT][from_][to]
 
     def nodes(self) -> set[Node]:
+        '''Get the nodes in the graph.'''
         return self._nodes
     
     def edges(self) -> Iterable[Edge]:
+        '''Get the edges in the graph.'''
         return itertools.chain.from_iterable(
             itertools.chain.from_iterable(
                 map(dict.values, self._edges[Direction.OUT].values())))
 
     def v(self) -> int:
+        '''Get the number of nodes (vertices) of the graph.'''
         return len(self._nodes)
 
     # def e(self) -> int: raise NotImplementedError
 
     def __bool__(self) -> bool:
+        '''A graph is false-y if it contains no nodes (and therefore no edges).'''
         return bool(self._nodes)
